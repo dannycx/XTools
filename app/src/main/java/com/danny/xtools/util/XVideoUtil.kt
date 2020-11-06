1b2604d1f31c80e7a1a91c919979266b0d852d06
unknown <danny>
2020-11-06 13:03:16 +0800

    视频工具

---
+++ app/src/main/java/com/danny/xtools/util/XVideoUtil.kt
@@ -0,0 +1,41 @@
+package com.danny.xtools.util
+
+import android.app.Activity
+import android.net.Uri
+import android.widget.VideoView
+
+/**
+ * 视频播放
+ *
+ * @author danny
+ * @since 2020-11-06
+ */
+object XVideoUtil {
+
+    fun setUri(activity: Activity, rawId: Int, video: VideoView) {
+        video.setVideoURI(Uri.parse("android.resource://${activity.packageName}/${rawId}"))
+    }
+
+    fun play(video: VideoView) {
+        if (!video.isPlaying) {
+            video.start()
+        }
+    }
+
+    fun pause(video: VideoView) {
+        if (video.isPlaying) {
+            video.pause()
+        }
+    }
+
+    fun replay(video: VideoView) {
+        if (video.isPlaying) {
+            video.resume()
+        }
+    }
+
+    fun destroy(video: VideoView) {
+        video.suspend()
+    }
+
+}
